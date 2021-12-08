1135adbac977dd8b818c35690dc9d26d6ea55259
wiryadev <abrarwiryawan@gmail.com>
2021-12-08 13:48:56 +0700

    add coroutines related rule

--- features/movies/src/test/java/isfaaghyth/app/movies/InstantTaskExecutorRuleSpek.kt
+++ features/movies/src/test/java/isfaaghyth/app/movies/InstantTaskExecutorRuleSpek.kt
@@ -2,11 +2,21 @@ package isfaaghyth.app.movies
 
 import androidx.arch.core.executor.ArchTaskExecutor
 import androidx.arch.core.executor.TaskExecutor
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.ObsoleteCoroutinesApi
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
 import org.spekframework.spek2.dsl.Root
 
+@ExperimentalCoroutinesApi
+@ObsoleteCoroutinesApi
 class InstantTaskExecutorRuleSpek(root: Root) {
+    private val dispatchers = newSingleThreadContext("test")
     init {
         root.beforeGroup {
+            Dispatchers.setMain(dispatchers)
             ArchTaskExecutor.getInstance().setDelegate(object : TaskExecutor() {
                 override fun executeOnDiskIO(runnable: Runnable) {
                     runnable.run()
@@ -21,6 +31,8 @@ class InstantTaskExecutorRuleSpek(root: Root) {
         }
         root.afterGroup {
             ArchTaskExecutor.getInstance().setDelegate(null)
+            Dispatchers.resetMain()
+            dispatchers.close()
         }
     }
 }
